73b808fd9b7541718cd2f9c583c284355bd42774
BriqueAuBob <briqueaubob@gmail.com>
2024-01-14 19:10:37 +0100

    :sparkles: Delete button

--- assets/js/delete.js
+++ assets/js/delete.js
@@ -1,12 +1,23 @@
-
-const deleteButton = document.querySelectorAll('.delete');
-
-deleteButton.forEach(button => {
-    button.addEventListener('click', () => {
-        fetch('/homework/' + button.dataset.homeworkId, {
-            method: 'DELETE',
+const deleteAction = (elements, callback) => {
+    elements.forEach(button => {
+        button.addEventListener('click', () => {
+            callback(button)
         });
-        document.getElementById('card-homework-' + button.dataset.homeworkId).remove();
-        button.remove();
     });
+}
+
+deleteAction(document.querySelectorAll('.delete'), (button) => {
+    fetch('/homework/' + button.dataset.homeworkId, {
+        method: 'DELETE',
+    });
+    document.getElementById('card-homework-' + button.dataset.homeworkId).remove();
+    button.remove();
+})
+
+deleteAction(document.querySelectorAll('.delete-notification'), (button) => {
+    fetch('/notification/' + button.dataset.notificationId, {
+        method: 'DELETE',
+    });
+    document.getElementById('card-notification-' + button.dataset.notificationId).remove();
+    button.remove();
 });
