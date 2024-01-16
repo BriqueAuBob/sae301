4025285d49ac1108d7809c9c180a08d5131e5ccd
BriqueAuBob <briqueaubob@gmail.com>
2024-01-16 10:52:58 +0100

    :construction: WIP Modal

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,7 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
+import ModalController from './controllers/modal-controller.js';
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+app.register('modal', ModalController)
--- src/Components/SearchHomework.php
+++ src/Components/SearchHomework.php
@@ -2,8 +2,11 @@
 
 namespace App\Components;
 
+use App\Entity\Homework;
 use App\Repository\HomeworkRepository;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
+use Symfony\UX\LiveComponent\Attribute\LiveListener;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
@@ -21,6 +24,9 @@ class SearchHomework
     #[LiveProp(writable: true)]
     public string $query = '';
 
+    #[LiveProp(writable: true)]
+    public ?Homework $homework = null;
+
     public function __construct(private HomeworkRepository $homeworkRepository)
     {
     }
@@ -28,5 +34,11 @@ class SearchHomework
     public function getHomeworks(): array
     {
         return $this->homeworkRepository->search($this->query);
+    }
+
+    #[LiveListener('setHomework')]
+    public function setHomework(#[LiveArg] ?Homework $homework): void
+    {
+        $this->homework = $homework;
     }
 }
