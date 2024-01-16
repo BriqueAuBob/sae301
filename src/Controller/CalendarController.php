ae196927f33ef9615e1c151619196025bfd7317e
Evan <94895335+WarLay0@users.noreply.github.com>
2024-01-16 10:07:42 +0100

    :rocket: | Mise en fonction de la bdd

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Homework;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class CalendarController extends AbstractController
 {
     #[Route('/calendar', name: 'app_calendar')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Récupération des homeworks
+        $homeworks = $entityManager->getRepository(Homework::class)->findAll();
+
         return $this->render('calendar/index.html.twig', [
             'controller_name' => 'CalendarController',
+            'homeworks' => $homeworks,
         ]);
     }
 }
