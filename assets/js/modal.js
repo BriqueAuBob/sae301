c4c0d06b04b884c942b9e9ddcb521f4b840529ff
BriqueAuBob <briqueaubob@gmail.com>
2024-01-17 15:16:26 +0100

    :dizzy: Modal animation

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -6,6 +6,11 @@ modals.forEach((element) => {
         const modal = document.getElementById(target);
         modal.classList.toggle('opacity-0');
         modal.classList.toggle('pointer-events-none');
+
+        const modalContent = modal.querySelector('.modal-content');
+        modalContent.classList.toggle('translate-y-8');
+        modalContent.classList.toggle('-translate-y-1/2');
+        modalContent.classList.toggle('scale-75');
     });
 });
 
@@ -17,5 +22,10 @@ closeButtons.forEach((element) => {
         const modal = document.getElementById(target);
         modal.classList.toggle('opacity-0');
         modal.classList.toggle('pointer-events-none');
+
+        const modalContent = modal.querySelector('.modal-content');
+        modalContent.classList.toggle('translate-y-8');
+        modalContent.classList.toggle('-translate-y-1/2');
+        modalContent.classList.toggle('scale-75');
     });
 });
