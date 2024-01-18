f0c086f1281e75c1d7066e38937294a5973ac9ca
Evan <94895335+WarLay0@users.noreply.github.com>
2024-01-18 09:32:18 +0100

    :rocket: | CalendarModal

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -47,22 +47,70 @@ $(document).ready(function(){
     calendar.setOption('locale', 'fr');
     calendar.render();
 })
-function toggleModaleCalendar(selector){
-    selector.toggleClass('opacity-0', 'opacity-1');
-    selector.toggleClass('pointer-events-none', 'pointer-events-auto');
-    selector.find('.modal-content').toggleClass('translate-y-8', 'translate-y-0');
-    selector.find('.modal-content').toggleClass('-translate-y-1/2', 'translate-y-0');
-    selector.find('.modal-content').toggleClass('scale-75', 'scale-100');
+
+// Changement de l'état de la modale'
+function toggleModaleCalendar(selector, state){
+    if(state === true){
+        setTimeout(function(){
+            // Opacity
+            selector.addClass('opacity-1');
+            selector.removeClass('opacity-0');
+
+            // Pointer events
+            selector.addClass('pointer-events-auto');
+            selector.removeClass('pointer-events-none');
+
+            // Translate
+            selector.find('.modal-content').addClass('-translate-y-1/2');
+            selector.find('.modal-content').removeClass('translate-y-8');
+            console.log(selector.find('.modal-content'));
+
+            // Scale
+            selector.find('.modal-content').addClass('scale-100');
+            selector.find('.modal-content').removeClass('scale-75');
+        }, 100)
+        console.log('true');
+    }else if(state === false){
+        // Opacity
+        selector.addClass('opacity-0');
+        selector.removeClass('opacity-1');
+
+        // Pointer events
+        selector.addClass('pointer-events-none');
+        selector.removeClass('pointer-events-auto');
+
+        // Translate
+        selector.find('.modal-content').addClass('translate-y-8');
+        selector.find('.modal-content').removeClass('translate-y-0');
+
+        // Scale
+        selector.find('.modal-content').addClass('scale-75');
+        selector.find('.modal-content').removeClass('scale-100');
+
+        console.log('false');
+    }
+
 }
+// Ouverture de la modale
 function modalCalendar(divCalendar) {
-    let dc = divCalendar;
-    let dcId = $(dc).attr('id');
-    let selector = $('#'+dcId);
-    console.log(dcId);
-    if (dcId === 'modal-homework-calendar') {
-        toggleModaleCalendar(selector);
-        console.log('oui');
-    }else {
-        console.log('non');
+    let target = $(divCalendar).attr('id');
+    let selector = $('#'+target);
+    console.log(target);
+    if (target === 'modal-homework-calendar') {
+        toggleModaleCalendar(selector, true);
     }
 }
+
+// Fermeture de la modale
+$("#CalendarModale").click(function(event){
+    if(event.target.classList.contains('closeModalButton')){
+        let selector = $("#modal-homework-calendar");
+        toggleModaleCalendar(selector, false);
+    }
+    if($(event.target).hasClass('bgCloseModal') ){
+        let selector = $("#modal-homework-calendar");
+        toggleModaleCalendar(selector, false);
+        // console.log('test');
+    }
+    console.log(event.target);
+})
