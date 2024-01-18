fcfda5b8cd2c022d5d5329c22be471821c480a09
Guillaume Gorvel <ggorvel@gmail.com>
2024-01-18 11:27:00 +0100

    notification UPDATE compteur dynamique

--- assets/js/isRead.js
+++ assets/js/isRead.js
@@ -1,15 +1,21 @@
 let notificationElements = document.querySelectorAll('.notification');
-
+const notificationCount = document.getElementById('readNotificationsCount');
+console.log(notificationCount);
 notificationElements.forEach(function (notificationElement) {
     notificationElement.addEventListener('click', function () {
+
+        if(notificationElement.classList.contains('opacity-50')){
+            return;
+        }
         let notificationId = notificationElement.dataset.notificationId;
 
-        console.log('test:', notificationId);
+        notificationCount.innerHTML = parseInt(notificationCount.innerHTML) - 1;
 
         fetch('/notification/' + notificationId, {
             method: 'PATCH',
         });
-        document.getElementById('card-notification-' + notificationId).style.opacity = "0.5";
+        notificationElement.classList.add('opacity-50') ;
+
 
     });
 });
