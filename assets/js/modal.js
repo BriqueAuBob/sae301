88cdfea63cf6a05844b909222fcd50f1f2f85fd1
BriqueAuBob <briqueaubob@gmail.com>
2024-01-18 15:59:12 +0100

    :lipstick: Fix color icons

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,43 +1,47 @@
 import { deleteAction } from './delete.js';
 
-const modals = document.querySelectorAll('[data-toggle="modal"]');
-modals.forEach((element) => {
-    element.addEventListener('click', (event) => {
-        event.preventDefault();
-        const target = element.dataset.modalId;
-        const modal = document.getElementById(target);
-        modal.classList.toggle('opacity-0');
-        modal.classList.toggle('pointer-events-none');
+const getModals = () => {
+    const modals = document.querySelectorAll('[data-toggle="modal"]');
+    modals.forEach((element) => {
+        element.addEventListener('click', (event) => {
+            event.preventDefault();
+            const target = element.dataset.modalId;
+            const modal = document.getElementById(target);
+            modal.classList.toggle('opacity-0');
+            modal.classList.toggle('pointer-events-none');
 
-        fetch(element.dataset.contentUrl).then((res) => res.text()).then((res) => {
-            modal.querySelector("#" + target + '-content').innerHTML = res;
-            loopDropdowns();
-            getCloseButtons();
-            deleteAction(document.querySelectorAll('.delete'), (button) => {
-                fetch('/homework/' + button.dataset.homeworkId, {
-                    method: 'DELETE',
-                });
-                document.getElementById('card-homework-' + button.dataset.homeworkId).remove();
-                button.remove();
+            fetch(element.dataset.contentUrl).then((res) => res.text()).then((res) => {
+                modal.querySelector("#" + target + '-content').innerHTML = res;
+                loopDropdowns();
+                getCloseButtons();
+                getModals();
+                deleteAction(document.querySelectorAll('.delete'), (button) => {
+                    fetch('/homework/' + button.dataset.homeworkId, {
+                        method: 'DELETE',
+                    });
+                    document.getElementById('card-homework-' + button.dataset.homeworkId).remove();
+                    button.remove();
 
-                modalContent.classList.toggle('translate-y-8');
-                modalContent.classList.toggle('-translate-y-1/2');
-                modalContent.classList.toggle('scale-75');
-                modalContent.classList.toggle('opacity-0');
-                setTimeout(() => {
-                    modal.classList.toggle('opacity-0');
-                    modal.classList.toggle('pointer-events-none');
-                }, 300)
+                    modalContent.classList.toggle('translate-y-8');
+                    modalContent.classList.toggle('-translate-y-1/2');
+                    modalContent.classList.toggle('scale-75');
+                    modalContent.classList.toggle('opacity-0');
+                    setTimeout(() => {
+                        modal.classList.toggle('opacity-0');
+                        modal.classList.toggle('pointer-events-none');
+                    }, 300)
+                })
             })
-        })
 
-        const modalContent = modal.querySelector('.modal-content');
-        modalContent.classList.toggle('translate-y-8');
-        modalContent.classList.toggle('-translate-y-1/2');
-        modalContent.classList.toggle('scale-75');
-        modalContent.classList.toggle('opacity-0');
+            const modalContent = modal.querySelector('.modal-content');
+            modalContent.classList.toggle('translate-y-8');
+            modalContent.classList.toggle('-translate-y-1/2');
+            modalContent.classList.toggle('scale-75');
+            modalContent.classList.toggle('opacity-0');
+        });
     });
-});
+}
+getModals();
 
 const getCloseButtons = () => {
 const closeButtons = document.querySelectorAll('[data-close-modal]');
--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -16,32 +16,24 @@ class CommentController extends AbstractController
     #[Route('/comment/{homework}', name: 'app_comment')]
     public function index(EntityManagerInterface $em, Request $request, Homework $homework): Response
     {
-        // Créer un nouveau commentaire
         $comment = new Comment();
 
-        // Créer le formulaire
-        $form = $this->createForm(CommentType::class, $comment);
+        $form = $this->createForm(CommentType::class, $comment, [
+            'action' => $this->generateUrl('app_comment', ['homework' => $homework->getId()]),
+            'method' => 'POST',
+        ]);
 
         // Gérer la soumission du formulaire
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Ajouter le contenu du commentaire
             $content = $form->get('content')->getData();
 
-            // Ajouter l'utilisateur connecté comme auteur
             $comment->setAuthor($this->getUser());
-
-            // Ajouter la date de création
             $comment->setCreatedAt(new \DateTime());
-
-            // Ajouter le commentaire au devoir
             $comment->setHomework($homework);
-
-            // Ajouter le contenu du commentaire récupéré du formulaire
             $comment->setContent($content);
 
-            // Enregistrer le commentaire dans la base de données
             $em->persist($comment);
             $em->flush();
         }
