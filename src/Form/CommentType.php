ad522ef5c088f03d5aac7cd401a6e2732a8e3d96
Guillaume Gorvel <ggorvel@gmail.com>
2024-01-20 19:08:51 +0100

    UPDATE label

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -7,6 +7,7 @@ use App\Entity\Homework;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +16,12 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
+            ->add('content', TextType::class, [
+                'label' => 'Commentaire',
+                'attr' => [
+                    'placeholder' => 'Votre commentaire',
+                ],
+            ])
         ;
     }
 
